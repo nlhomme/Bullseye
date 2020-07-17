bdd7b20168043f5b78b6458c71a5c5d0a31c42cd
nlhomme <9362475+nlhomme@users.noreply.github.com>
2020-07-18 00:58:36 +0200

    Syntax correction

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -13,27 +13,26 @@ struct ContentView: View {
     @State var alertIsVisible: Bool = false
 
     var body: some View {
-        VStack {
-            VStack {
-                Text("Hello, World!")
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.green)
-                Button(action: {
-                    print("Button pressed")
-                    self.alertIsVisible = true
-                }) {
-                    Text("Hit me!")
-                }
-                .alert(isPresented: $alertIsVisible) { () ->
-                    Alert in
-                    return Alert(title: Text("Hello there!"),
-                                 message: Text("This is my first pop-up!"),
-                                 dismissButton: .default(Text("Awesome!")))
+                VStack {
+                    Text("Hello, World!")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.green)
+                    Button(action: {
+                        print("Button pressed")
+                        self.alertIsVisible = true
+                    }) {
+                        Text("Hit me!")
+                    }
+                    .alert(isPresented: $alertIsVisible) { () ->
+                        Alert in
+                        return Alert(title: Text("Hello there!"),
+                                     message: Text("This is my first pop-up!"),
+                                     dismissButton: .default(Text("Awesome!")))
+                    
                 }
             }
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
