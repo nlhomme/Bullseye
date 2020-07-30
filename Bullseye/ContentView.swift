bc638dfe1349b76058d876292da8f7b76640198b
Nicolas Lhomme <nicolas.lhomme@protonmail.com>
2020-07-30 20:51:23 +0200

    Suppression des modifications et stylisation des boutons pour la suite du cours

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,16 +11,12 @@ import SwiftUI
 struct ContentView: View {
 
     @State var alertIsVisible: Bool = false
-    @State var knockAlertIsVisible: Bool = false
 
     var body: some View {
         VStack {
             Text("Hello, World!")
                 .fontWeight(.semibold)
                 .foregroundColor(Color.green)
-
-            Spacer()
-                .frame(height: 50)
 
             Button(action: {
                 print("Button pressed")
@@ -34,46 +30,7 @@ struct ContentView: View {
                     message: Text("This is my first pop-up!"),
                     dismissButton: .default(Text("Awesome!")))
             }
-
-            Spacer()
-                .frame(height: 50)
-
-            Button(action: {
-                print("User wants a knock kock joke")
-                self.knockAlertIsVisible = true
-            })
-                {
-                HStack {
-                    Image(systemName: "person.icloud.fill")
-                    Text("Knock knock!")
-                }
-            }
-            .buttonStyle(GradientButtonStyle())
-            .frame(alignment: .topLeading)
-            .alert(isPresented: $knockAlertIsVisible) { () ->
-                Alert in
-                return Alert(title: Text("Who's there?"),
-                    message: Text("Tank"),
-                    dismissButton: .default(Text("Tank who?")))
-            }
         }
-    }
-}
-
-// Déclaration d'un style de bouton
-struct GradientButtonStyle: ButtonStyle {
-    func makeBody(configuration: Self.Configuration) -> some View {
-        configuration.label
-            // Couleur du premier plan (le texte)
-            .foregroundColor(Color.white)
-            // Rembourrage du bouton
-            .padding()
-            // Propriété de l'arrière plan
-            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
-            // Arrondissement des coins du bouton
-            .cornerRadius(15.0)
-            // Effet de rembondissement quand on appuie sur le bouton
-            .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
     }
 }
 
