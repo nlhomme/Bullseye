20f52bdec224bf78b247db42e126a228da31d944
Nicolas Lhomme <nicolas.lhomme@protonmail.com>
2020-08-22 00:43:05 +0200

    Partie 4.36: Challenge: Bonus Points

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -88,8 +88,19 @@ struct ContentView: View {
     }
     
     func pointsForCurrentRound() -> Int {
-        //return 100 - abs(target - sliderValueRounded())
-        return 100 - amountOff()
+        let maximumScore = 100
+        let difference = amountOff()
+        let bonus: Int
+        
+        if difference == 0 {
+            bonus = 100
+        } else if difference == 1 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+        
+        return (maximumScore - difference) + bonus
     }
     
     func alertTitle() -> String {
