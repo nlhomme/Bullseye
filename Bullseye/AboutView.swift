892451d0991a8f56ad44996b57048524cdd10216
Nicolas Lhomme <nicolas.lhomme@protonmail.com>
2020-08-23 20:51:46 +0200

    Partie 5.45: Challenge: Styling the About Page

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -9,15 +9,43 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        VStack{
-        Text("🎯 Bullseye 🎯")
-        Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
-        Text("Your goal is to place the slider as close as you can to the target value. The closer you are, the more points you score.")
-        Text("''Your first iOS and SwiftUI App'' course follow up on raywenderlish.com")
-        Text ("Nicolas Lhomme, 08/23/2020")
+    
+    let beige = Color(red: 255.0 / 255.0, green: 214.0 / 255.0, blue: 179.0 / 255)
+    
+    struct HeadingStyle: ViewModifier {
+        func body (content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 30))
+                .padding(.top, 20)
+                .padding(.bottom, 20)
         }
-        .navigationBarTitle("About Bullseye")
+    }
+    
+    struct BodyStyle: ViewModifier {
+        func body (content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .padding(.leading, 60)
+                .padding(.trailing, 60)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    var body: some View {
+        Group{
+            VStack{
+                Text("🎯 Bullseye 🎯").modifier(HeadingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(BodyStyle())
+                Text("Your goal is to place the slider as close as you can to the target value. The closer you are, the more points you score.").modifier(BodyStyle())
+                Text("''Your first iOS and SwiftUI App'' course follow up on raywenderlish.com").modifier(BodyStyle())
+                Text ("Nicolas Lhomme, 08/23/2020").modifier(BodyStyle())
+            }
+            .navigationBarTitle("About Bullseye")
+            .background(beige)
+        }
+        .background(Image("Background"), alignment: .center)
     }
 }
 
